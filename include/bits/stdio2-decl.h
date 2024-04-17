seanpm2001/glibc

Enable --enable-fortify-source with clang

include/bits/stdio2-decl.h
@@ -1,1 +1,8 @@
 #include <libio/bits/stdio2-decl.h>
+
+#ifndef _ISOMAC
+libc_hidden_ldbl_proto (vfprintf)
+libc_hidden_proto (__fgets_unlocked_chk)
+libc_hidden_proto (__vasprintf_chk)
+libc_hidden_proto (__vfprintf_chk)
+#endif

reposyncer/gnu-libc

include/bits/stdio2-decl.h
@@ -1,1 +1,8 @@
 #include <libio/bits/stdio2-decl.h>
+
+#ifndef _ISOMAC
+libc_hidden_proto (__fgets_unlocked_chk)
+libc_hidden_ldbl_proto (vfprintf)
+libc_hidden_ldbl_proto (__vasprintf_chk)
+libc_hidden_ldbl_proto (__vfprintf_chk)
+#endif